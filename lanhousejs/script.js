3f0716abc97296c995679d92a5361168e0daf2a3
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-08-20 17:44:29 -0300

    att.lanhousejs

--- lanhousejs/script.js
+++ lanhousejs/script.js
@@ -2,7 +2,18 @@ const valorDeUso = 3;
 let tempoDeUso = document.getElementById("usotxt");
 let res = document.getElementById("res");
 
+
 function calcular() {
     const valor =  tempoDeUso.value * 0.2;
     res.innerHTML = `Valor a pagar: R$ ${valor.toFixed(2)}`;
 }
+
+tempoDeUso.addEventListener('keypress',  (e) => {
+    console.log(e)
+    if(e.code === 'Enter') {
+        calcular()
+    } 
+    if (e.code === 'KeyC') {
+        res.innerHTML = 'Parabéns, ao apertar a tecla C vc descobriu um easterEgg, a carol é a mulher mais linda do mundo';
+    }
+});
